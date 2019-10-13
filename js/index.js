152d679e5da5aa0aed03ccbe90a3e043be1415f8
rasheedshina22 <rasheedshinaopeyemi@gmail.com>
2019-10-13 15:58:24 +0100

    initial commit

---
+++ js/index.js
@@ -0,0 +1,67 @@
+const selectedQuestion = questions
+  .sort(() => {
+    return Math.random() - Math.random();
+  })
+  .slice(0, 5);
+let score = 0;
+let questionIndex = 0;
+
+const gradeQuestion = answ => {
+  if (answ == selectedQuestion[questionIndex].answer) {
+    score += 20;
+  }
+  questionIndex += 1;
+};
+
+console.log(questions);
+
+const renderSuccess = () => {
+  $(".question-number").text("");
+  $(".question-question").text("");
+  $(".option1").text("");
+  $(".option2").text("");
+  $(".option3").text("");
+  $(".option4").text("");
+  if (score >= 50) {
+    $(".score").html(` Well done! You scored ${score}`);
+  } else {
+    $("<p>You can do it. Try again </p>").appendTo($(".question-question"));
+
+    $(".score").html(` Hmmmmm! You scored ${score}`);
+  }
+};
+
+const renderQuestion = qindex => {
+  let question = selectedQuestion[qindex];
+  $(".question-number").text(`Question ${qindex + 1}`);
+  $(".question-question").text(question.question);
+  $(".option1").text("");
+  $(".option2").text("");
+  $(".option3").text("");
+  $(".option4").text("");
+  //   since we are using jquery
+  $.each(question.options, (index, option) => {
+    let opt = ".option" + (index + 1);
+    let radioInput = $(
+      '<input type="radio" name="answer" value=' + option + " />"
+    );
+    let radioLabel = $("<label for=" + option + "> " + option + " </label>");
+    radioInput.appendTo(opt);
+
+    radioLabel.appendTo(opt);
+  });
+
+  $('input:radio[name="answer"]').click(function() {
+    //grade the question
+    gradeQuestion(this.value);
+    if (questionIndex == selectedQuestion.length) {
+      renderSuccess();
+    } else {
+      renderQuestion(questionIndex);
+    }
+  });
+};
+
+$(document).ready(function() {
+  renderQuestion(questionIndex);
+});
---
+++ js/questions.js
@@ -0,0 +1,52 @@
+const questions = [
+  {
+    question: "which of the following is not an English team",
+    options: ["Chelsea", "Man United", "Man City", "Barcelona"],
+    answer: "Barcelona"
+  },
+  {
+    question: "which team won the last champions league",
+    options: ["Real Madrid", "Barcelona", "Liverpool", "Chelsea"],
+    answer: "Liverpool"
+  },
+  {
+    question: "Which country won the last world cup",
+    options: ["France", "England", "Nigeria", "Spain"],
+    answer: "France"
+  },
+  {
+    question: "which of these players won the last fifa best player",
+    options: ["L Messi", "C Ronaldo", "F Ribery", "E Hazard"],
+    answer: "L Messi"
+  },
+  {
+    question: "Which of this team is not a spanish team",
+    options: ["Real Madrid", "Chelsea", "Barcelona", "Sevilla"],
+    answer: "Chelsea"
+  },
+  {
+    question: "Which country has won the world cup more",
+    options: ["Germany", "France", "Brazil", "Spain"],
+    answer: "Brazil"
+  },
+  {
+    question: "Who is currently the coach of Chelsea Fc",
+    options: ["F Lampard", "R Shina", "M Owen", "J Klop"],
+    answer: "F Lampard"
+  },
+  {
+    question: "Which team won the last english premier league competition",
+    options: ["Man United", "Chelsea", "Man City", "Liverpool"],
+    answer: "Man City"
+  },
+  {
+    question: "Ngolo Kante is a player at which of these Clubs",
+    options: ["Liverpool", "Chelsea", "Man United", "Man city"],
+    answer: "Chelsea"
+  },
+  {
+    question: "M Salah is a player at which of these Clubs",
+    options: ["Liverpool", "Chelsea", "Man United", "Man City"],
+    answer: "Liverpool"
+  }
+];
